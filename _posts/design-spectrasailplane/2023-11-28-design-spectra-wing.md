8bedd0232dcd700266da5bee938397073d3bd232
evgenvandev <developjev@gmail.com>
2023-12-15 14:30:07 +0500

    First_commit_Spectrasailplane

---
+++ _posts/design-spectrasailplane/2023-11-28-design-spectra-wing.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title: Проектируем крыло для Spectra
+feature-img: "assets/img/spectra-sailplane/design-spectra-wing/spectra-wing-1.png"
+thumbnail: "assets/img/thumbnails/spectra-sailplane/design-spectra-wing/spectra-wing-2.png"
+tags: [AirCraftModel, Spectra]
+categories: AirCraftModel
+---
+
---
+++ _sass/base/1.java
@@ -0,0 +1,23 @@
+// 3-я тема
+// Очень светлая тема.
+/* BASE16 CODE HIGHLIGHT */
+// Harmonic16 Light Theme
+//https://github.com/chriskempson/base16-unclaimed-schemes/blob/master/harmonic-light.yaml
+html, html[data-theme="light"] {
+  --base00: #f7f9fb; // Default Background
+  --base01: #e5ebf1; // Lighter Background (Used for status bars) - NOT USED
+  --base02: #cbd6e2; // Selection Background
+  --base03: #aabcce; // Comments, Invisible, Line Highlighting
+  --base04: #627e99; // Dark Foreground (Used for status bars)
+  --base05: #405c79; // Punctuation
+  --base06: #223b54; // Highlight, Light Background
+  --base07: #0b1c2c; // Default Foreground, Default text color
+  --base08: #bf8b56; // Operators (and Parenthesis, Caret, Delimiters)
+  --base09: #bfbf56; // Classes, Markup Bold, Search Text Background
+  --base0a: #8bbf56; // Strings, Inherited Class, Markup Code, Diff Inserted
+  --base0b: #56bf8b; // Functions, Methods, Attribute IDs, Headings
+  --base0c: #568bbf; // Support, Regular Expressions, Escape Characters, Markup Quotes
+  --base0d: #8b56bf; // Keywords, Storage, Selector, Markup Italic, Diff Changed
+  --base0e: #bf568b; // Integers, Boolean, Constants, XML Attributes, Markup Link Url
+  --base0f: #bf5656; // Deprecated, Opening/Closing Embedded Language Tags e.g.
+}
